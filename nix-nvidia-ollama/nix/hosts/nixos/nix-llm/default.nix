956679e50245f3764f4a37cc7319af96c6fbf369
GaryGG <gglab100@gmail.com>
2025-09-29 17:59:28 +1000

    nix ollama

---
+++ nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
@@ -0,0 +1,81 @@
+{ config, inputs, pkgs, name, lib, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./../../common/nixos-common.nix
+      ./../../common/common-packages.nix
+    ];
+
+  # Boot configuration
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  # Network configuration
+  networking = {
+    firewall.enable = false;
+    hostName = "nix-ollama";
+    interfaces.ens18 = {
+      useDHCP = false;
+      ipv4.addresses = [{
+        address = "192.168.0.151";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = "192.168.0.1";
+    nameservers = [ "192.168.0.1" ];
+  };
+
+  # System localization
+  time.timeZone = "Australia/Sydney";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services.xserver = {
+    enable = false;
+    videoDrivers = [ "nvidia" ];
+  };
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+    settings.PermitRootLogin = "yes";
+  };
+  services.qemuGuest.enable = true;
+  services.tailscale.enable = true;
+  # services.ollama = {
+  #   enable = true;
+  #   host = "0.0.0.0";
+  # };
+
+  # userland
+  #home-manager.useGlobalPkgs = true;
+  #home-manager.useUserPackages = true;
+  #home-manager.users.zaphod = { imports = [ ./../../../home/zaphod.nix ]; };
+  users.users.zaphod = {
+    isNormalUser = true;
+    description = "zaphod";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      #home-manager
+    ];
+  };
+
+  # Hardware configuration
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+      open = false;
+      nvidiaSettings = true;
+      powerManagement.enable = true;
+    };
+    nvidia-container-toolkit.enable = true;
+  };
+
+}
